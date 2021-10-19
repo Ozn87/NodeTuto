f68fa9d2217ce7a0e2513bc27d83ac4c332e1c04
Osiris Loera <loera.osiris@gmail.com>
2021-10-18 20:38:43 -0600

    Added Live server

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-/*
 const http = require('http')
 const path = require('path')
 const fs = require('fs')
 
 const server = http.createServer((req, res) => {
     //console.log(req.url)
-    if (req.url === '/') {
+   /* if (req.url === '/') {
 
 
         fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
@@ -21,10 +20,61 @@ const server = http.createServer((req, res) => {
         ];
      res.writeHead(200, {'Content-Type': 'application/json'});
      res.end(JSON.stringify(users));
-    };
+    };*/
+
+    //Build dynamic file path                   resolves path to file requested by using ternery operators
+    let filePath = path.join(__dirname,'public',req.url==='/'?'index.html':req.url);
+
+    /*console.log(filePath)
+    res.end();*/
+
+    let exName=path.extname(filePath);
+
+    //initial content type
+    let contentType = 'text/html'
+    //check the ext to set content type
+    switch(exName){
+        case '.js':
+            contentType ='text/javascript';
+            break;
+        case '.css':
+            contentType='text/css';
+            break;
+        case '.json':
+            contentType='application/json';
+            break;
+        case'.jpg':
+            contentType='image/jpg';
+            break;
+        case '.png':
+            contentType='image/png';
+            break;
+    }
+
+
+    //read file
+    fs.readFile(filePath, (err,content)=>{
+       if(err){
+           if(err.code =='ENOENT'){
+               //page not found
+               fs.readFile(path.join(__dirname,'public','404.html'), (err,content)=>{
+                   res.writeHead(200, {'Content-Type': 'text/html'})
+                   res.end(content,'utf8');
+               })
+           }else{
+               //some server error
+               res.writeHead(500)
+               res.end(`Server error ${err.code}`)
+           }
+       }else{
+           //success
+           res.writeHead(200, {'Content-Type': contentType})
+           res.end(content,'utf8');
+       }
+    });
+
 });
 
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`SERVER RUNNING IN PORT: ${PORT}`))
-*/
